72f804e8b09b33f190387e17f1988cb6a6a8237e
Rafael José <107479165+hvemerjeg@users.noreply.github.com>
2022-06-24 12:18:22 +0200

    Update Caesar_cipher.py

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,5 +1,5 @@
 #THIS CODE IS AN EASY IMPLEMENTATION OF THE CAESAR CIPHER
-#If you do not know how the Caesar cipher works check out the description given by Wikipedia(I found it very helpful):https://en.wikipedia.org/wiki/Caesar_cipher:
+#If you do not know how the Caesar cipher works check out the description given by Wikipedia(I found it very helpful):https://en.wikipedia.org/wiki/Caesar_cipher
 def my_cifrado_caesar_2(string, llave):
     my_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'#My string where the alphabet in use is stored.
     indice = []#I will make use of this empty list to store the index that each letter has in the alphabet(my_alphabet).
@@ -7,7 +7,7 @@ def my_cifrado_caesar_2(string, llave):
     for letter in string:#We iterate through the string and store the index that each letter has in the alphabet in the list(indice).
         indice.append(my_alphabet.index(letter))
     for i in indice:#We make the permutation adding the value of the key to the index that each letter has in the alphabet.
-            cifrado.append(my_alphabet[(i + llave) % 26])#We make use of the modulus 26 becasue each time that we pass through 26 we need to start from 0. For more information
+            cifrado.append(my_alphabet[(i + llave) % 26])#We make use of the modulus 26 because each time that we pass through 26 we need to start from 0. For more information
             #about modular arithmetic:https://en.wikipedia.org/wiki/Modular_arithmetic.
     print("".join(cifrado))#We join all the letters in the list called "cifrado". And we get our encrypted string.
 if __name__ == '__main__':
