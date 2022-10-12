3cf976e90a8bf981aea5f364966789450a187417
Rafael José <107479165+hvemerjeg@users.noreply.github.com>
2022-10-12 12:20:11 +0200

    Create validate_subsequence.js

---
+++ JavaScript/Well-known_algorithms/validate_subsequence.js
@@ -0,0 +1,11 @@
+function validSubSequence(arr, s) {
+	arr_index = 0;
+	s_index = 0;
+	while (arr_index < arr.length && s_index < s.length) {
+		if (s[s_index] == arr[arr_index]) {
+			s_index++;
+		}
+		arr_index++;
+	}
+	return s_index == s.length;
+}
