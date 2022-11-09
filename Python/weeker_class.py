24198675f0bc834089315104845c3c0de157d697
hvemerjeg <rafaelgulias@hotmail.com>
2022-11-09 11:11:44 +0100

    weeker_class.py

---
+++ Python/weeker_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class Weeker:
+	__weekdays = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+	def __init__(self, day: str):
+		assert day in Weeker.__weekdays, "\033[1;31;40mThe day is not in the right format or that day does not exist!\033[0m"
+		self.__day = day
+
+	def __str__(self):
+		return f"\033[1;36;40m{self.__day}\033[0m"
+
+	def addDays(self, n: int):
+		self.__day = Weeker.__weekdays[(Weeker.__weekdays.index(self.__day) + n) % len(Weeker.__weekdays)]
+	
+	def subtractDays(self, n: int):
+		self.__day = Weeker.__weekdays[(Weeker.__weekdays.index(self.__day) - n) % len(Weeker.__weekdays)]
+
+if __name__ == '__main__':
+	weekday = Weeker('Mon')
+	print(weekday)
+	weekday.addDays(8)
+	print(weekday)
+	weekday.subtractDays(1)
+	print(weekday)
