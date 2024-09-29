9cf2b83e6ff0c3c97bb9f91cef49907291e6df80
hvemerjeg <rafaelgulias@hotmail.com>
2024-09-29 11:58:16 -0400

    Modify caesar cipher code

--- Python/Cryptography/Caesar_cipher.py
+++ Python/Cryptography/Caesar_cipher.py
@@ -1,28 +1,40 @@
 #THIS CODE IS AN EASY IMPLEMENTATION OF THE CAESAR CIPHER
 #If you do not know how the Caesar cipher works check out the description given by Wikipedia(I found it very helpful):https://en.wikipedia.org/wiki/Caesar_cipher
-def my_cifrado_caesar_2(string, llave):
-    my_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'#My string where the alphabet in use is stored.
-    indice = []#I will make use of this empty list to store the index that each letter has in the alphabet(my_alphabet).
-    cifrado = []#I will use this empty list to store the index that each letter has in the alphabet + the key, so we get the permutation.
-    for letter in string:#We iterate through the string and store the index that each letter has in the alphabet in the list(indice).
-        indice.append(my_alphabet.index(letter))
-    for i in indice:#We make the permutation adding the value of the key to the index that each letter has in the alphabet.
-            cifrado.append(my_alphabet[(i + llave) % 26])#We make use of the modulus 26 because each time that we pass through 26 we need to start from 0. For more information
-            #about modular arithmetic:https://en.wikipedia.org/wiki/Modular_arithmetic.
-    print("".join(cifrado))#We join all the letters in the list called "cifrado". And we get our encrypted string.
-#A more compressed way
-def caesarCipher(plain_text: str, key: int):
-    plain_text = plain_text.upper()
-    alphabet = [chr(ch) for ch in range(ord('A'), ord('Z') + 1)]#This is a little trick to create our alphabet using python built-in functions. The chr function returns
-#the letter that corresponds to a decimal value, and the ord function returns the decimal value that corresponds to a letter. You can find all of these correlations 
-#between values and letters by searching for the ASCII table. 
-    cipher_text = ''
-    for i in range(len(plain_text)):
-        cipher_text += alphabet[(alphabet.index(plain_text[i]) + key) % len(alphabet)]
-    return cipher_text
+import sys
+class CaesarCipher:
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'# You can create this with the following bash command: for char in {A..Z}; do echo $char; done | xargs | tr -d ' ' 
+
+    def encrypt(plaintext: str, key: int) -> str:
+        ciphertext = ''
+        for char in plaintext:
+            if char not in CaesarCipher.alphabet:
+                ciphertext += char
+            else: 
+                ciphertext += CaesarCipher.alphabet[(CaesarCipher.alphabet.index(char) + key) % len(CaesarCipher.alphabet)]
+        return ciphertext
+
+        
+    def decrypt(ciphtext: str, key: int) -> str:
+        return CaesarCipher.encrypt(ciphtext, -key)
+
+def main():
+    if len(sys.argv) != 2:
+        '''
+        python3 Caesar_cipher.py enc
+        python3 Caesar_cipher dec
+        '''
+        sys.stderr.write(f'[ + ] Usage: {sys.argv[0]} enc/dec\n')
+        exit(1)
+    if sys.argv[1] == 'enc':
+        plaintext = input('Insert message to encrypt: ').upper()
+        key = int(input('Insert key: '))
+        ciphertext = CaesarCipher.encrypt(plaintext, key)
+        print(f'Ciphertext: {ciphertext}')
+    elif sys.argv[1] == 'dec':
+        ciphertext = input('Insert message to decrypt: ').upper()
+        key = int(input('Insert key: '))
+        plaintext = CaesarCipher.decrypt(ciphertext, key)
+        print(f'Plaintext: {plaintext}')
+
 if __name__ == '__main__':
-    my_string = input().upper()#We are making all the letters uppercase. This is just a preference.
-    llave = int(input())#The key defines how many steps to the right we are going to take to perform the encryption of our letters.
-    my_cifrado_caesar_2(my_string, llave)#We call the function.
-#This implementation of the Caesar cipher is not a very fancy one, but enough to show the fundamentals of the Caesar cipher and interesting concepts as modular arithmetic.
-    print(caesarCipher(my_string, llave))
+    main()
