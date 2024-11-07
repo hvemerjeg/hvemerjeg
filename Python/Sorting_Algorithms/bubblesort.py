a202b955acc5bd51a043cebab4bdd4f18b25d56e
hvemerjeg <rafaelgulias@hotmail.com>
2024-11-07 11:41:50 -0500

    Change variables name

--- Python/Sorting_Algorithms/bubblesort.py
+++ Python/Sorting_Algorithms/bubblesort.py
@@ -3,24 +3,25 @@
 #There are functions in different programming languages that helps you to sort an array/list and 
 #there is a sort algorithm behind those functions.
 #For example, behind the .sort() function in python the Timsort algorithm is applied.
-def bubblesort(arr):#This is our main function that will hold our bubble sort implementation.
-    número_de_comparaciones = (len(arr) - 1)#In the first pass we will need to make n - 1 comparisons, where n is the length of the list. This number is decreasing since 
-#after the first pass we have the highest number in the correct position. So after the first pass we will need to make n - 2 comparisons, after the third n - 3 comparisons etc..
-    swap = True#We will make use of this variable to check if there is a swap. If in one whole pass there has been no change we will know that our elements are ordered correctly. 
-    while número_de_comparaciones > 0 and swap == True:#So if there has been no change in a whole pass meaning that swap equals false or the number of comparisons is not higher
-#than 0 we know that we are done.
-        swap = False
-        for i in range(número_de_comparaciones):
-            if arr[i] > arr[i + 1]:#We compare the element in the position i with the element in the position i + 1. If the element in the position i is higher than the
-#element in the position i + 1 we swap them.
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]#swaping them if the above condition is met.
-                swap = True#Setting swap to true because there has been a change.
-        número_de_comparaciones -= 1#As I said before, after one whole pass through the array we have the highest number in the correct position, so we will need to make
-#one less comparison.
-    print(arr)#Getting our ordered list.
+class BubbleSort:
+    def sort(arr: list) -> list:#This is our main function that will hold our bubble sort implementation.
+        n_comparisons = (len(arr) - 1)#In the first pass we will need to make n - 1 comparisons, where n is the length of the list. This number is decreasing since 
+    #after the first pass we have the highest number in the correct position. So after the first pass we will need to make n - 2 comparisons, after the third n - 3 comparisons etc..
+        swapped = True#We will make use of this variable to check if there is a swap. If in one whole pass there has been no change we will know that our elements are ordered correctly. 
+        while n_comparisons > 0 and swapped == True:#So if there has been no change in a whole pass meaning that swap equals false or the number of comparisons is not higher
+    #than 0 we know that we are done.
+            swapped = False
+            for i in range(n_comparisons):
+                if arr[i] > arr[i + 1]:#We compare the element in the position i with the element in the position i + 1. If the element in the position i is higher than the
+    #element in the position i + 1 we swap them.
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]#swaping them if the above condition is met.
+                    swapped = True#Setting swap to true because there has been a change.
+            n_comparisons -= 1#As I said before, after one whole pass through the array we have the highest number in the correct position, so we will need to make
+    #one less comparison.
+        return arr#Getting our ordered list.
 
 
 
 if __name__ == '__main__':
-    my_array = list(map(int, input().split()))#Getting a list as input and converting the elements inside the list to integers.
-    bubblesort(my_array)#Calling the function.
+    my_array = list(map(int, input("Insert integers space separated: ").split()))#Getting a list as input and converting the elements inside the list to integers.
+    print(BubbleSort.sort(my_array))#Calling the function.
